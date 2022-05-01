611bc44c2e445c029856ddb839b83fdecb98fa8c
Yano Kohei <o83184206@gmail.com>
2022-05-01 09:20:31 +0900

    routes.rb_orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'orders/create'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       get 'foods/index'
     end
   end

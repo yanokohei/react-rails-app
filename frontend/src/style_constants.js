128de630c3fb9969b7187d326df081a35a17e106
Yano Kohei <o83184206@gmail.com>
2022-05-04 06:27:31 +0900

    スタイルの定数を定義

---
+++ frontend/src/style_constants.js
@@ -0,0 +1,13 @@
+// background-color: ${COLORS.SUB_BUTTON};などとすることができる
+export const COLORS = {
+  MAIN: "#02A457",
+  BORDER: "#e2e2e2",
+  SUB_TEXT: "#545454",
+  SUB_BUTTON: "#eeeeee",
+};
+
+export const FONT_SIZE = {
+  STAND_BODY: "24px",
+  BODY1: "16px",
+  BODY2: "14px",
+};

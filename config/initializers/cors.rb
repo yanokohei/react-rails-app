245720dfe24ff54bb5eb8cb4d1573098e2756bcc
Yano Kohei <o83184206@gmail.com>
2022-05-04 10:31:59 +0900

    rack-corsの設定(許可するサイトのoriginを追記)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001' # 許可するサイトのオリジンを追記
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head] # 許可するリソースやリクエスト
+  end # もし本番環境で必要のないメソッドがあれば追加しない方が安全
+end

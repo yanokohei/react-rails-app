8fd43cf1708e74b574f4c76e347e5c9d5ce7c8fa
Yano Kohei <o83184206@gmail.com>
2022-05-04 10:32:28 +0900

    Restaurants.jsx(API関数を使ってみる)

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,9 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import { fetchRestaurants } from "../apis/restaurants";
 
 export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants().then((data) => console.log(data));
+  }, []);
   return <Fragment>レストラン一覧</Fragment>;
 };

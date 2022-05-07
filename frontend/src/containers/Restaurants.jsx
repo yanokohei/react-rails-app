01ed8bc49c0cb20796ad77b427fad3e3479f5bd1
Yano Kohei <o83184206@gmail.com>
2022-05-07 13:34:05 +0900

    Restaurants.jsx(useReducerで試しに➕➖ボタンを実装してみる)

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import styled from "styled-components";
 
 // apis
@@ -26,16 +26,42 @@ const MainCover = styled.img`
   height: 600px;
 `;
 
+const YanoWrapper = styled.div`
+  text-align: center;
+`;
+
+const initialState = 0;
+
+function reducer(countState, action) {
+  switch (action) {
+    case "increment":
+      return countState + 1;
+    case "decrement":
+      return countState - 1;
+    case "reset":
+      return initialState;
+    default:
+      return countState;
+  }
+}
+
 export const Restaurants = () => {
   useEffect(() => {
     fetchRestaurants().then((data) => console.log(data));
   }, []);
 
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       <HeaderWrapper>
         <MainLogoImage src={MainLogo} alt="main logo" />
       </HeaderWrapper>
+      <YanoWrapper>
+        <h2>カウント{count}</h2>
+        <button onClick={() => dispatch("increment")}>➕</button>
+        <button onClick={() => dispatch("decrement")}>➖</button>
+      </YanoWrapper>
+
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>

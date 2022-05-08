1e80ffedc6837fd701437ab6f99433c32860e7a6
Yano Kohei <o83184206@gmail.com>
2022-05-08 12:13:13 +0900

    reducers/restaurants.js(APIリクエストに対するstateを作成するリデューサーを切り出す)

---
+++ frontend/src/reducers/restaurants.js
@@ -0,0 +1,33 @@
+// APIリクエストの定数をインポート
+import { REQUEST_STATE } from "../constants";
+
+// 初期のステートを定義する。APIリクエストしてレストランのリストのレスポンスを受け取るため空配列とする
+export const initialState = {
+  fetchState: REQUEST_STATE.INITIAL,
+  restaurantsList: [],
+};
+
+// 取得に関する状態を定義する。
+export const restaurantsActionTypes = {
+  FETCHING: "FETCHING",
+  FETCH_SUCCESS: "FETCH_SUCCESS",
+};
+
+// dispatchにFETCHINGが渡ってきた場合はstateをLOADINGに変更し
+// FETCH_SUCCESSを受け取った場合は200とレスポンス結果をオブジェクトで返す。
+export const restaurantsReducer = (state, action) => {
+  switch (action.type) {
+    case restaurantsActionTypes.FETCHING:
+      return {
+        ...state,
+        fetchState: REQUEST_STATE.LOADING,
+      };
+    case restaurantsActionTypes.FETCH_SUCCESS:
+      return {
+        fetchState: REQUEST_STATE.OK,
+        restaurantsList: action.payload.restaurants,
+      };
+    default:
+      throw new Error();
+  }
+};
